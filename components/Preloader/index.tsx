19a87c9edfd41e5721b9fc516f63bdbc1430ecfa
@enwerro <rock.enwerem@tecnotree.com>
2023-06-02 16:14:49 +0100

    update preloader

--- components/Preloader/index.tsx
+++ components/Preloader/index.tsx
@@ -3,12 +3,13 @@ const Preloader = () => {
     <section className="preloader fixed font-sans z-10 w-100 h-100 bg-gray-950 top-0 left-0 right-0 bottom-0">
       <div className="intro w-1/2 m-auto text-white mt-[15%]">
         <div className="intro-title">
-          with years of experience in building products and appealing web
-          experiences.
+          I&apos;m Rock Enwerem, a skilled frontend engineer.
           <br />
-          I&apos;ve collaborated with individuals and teams to build experiences
-          for SMEs and large enterprises including Pericius, Africa Aprudential,
-          Cashbox.
+          with years of experience in building products and appealing web
+          applications.
+          <br />
+          I&apos;ve collaborated with individuals and businesses in different
+          capacities to build to user friendly applications.
         </div>
       </div>
       <div className="count absolute text-5xl bottom-10 left-[50%] text-white">
